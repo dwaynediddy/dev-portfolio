110d005e5d6e41f66683181bd994a29def5fdada
dwaynediddy <burrows.dwayne@icloud.com>
2022-09-26 20:27:18 +0200

    changes foor dark mode coming

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,18 @@
 import Nav from './Nav'
 import Footer from './Footer'
 
+import { useState } from 'react'
+
 export default function Layout({ children }: any) {
+  const [mode, setMode] = useState(true)
+
   return (
-    <>
+    <div className="light">
+    {/* <div className={mode ? "dark" : "light"}> */}
       <Nav />
       <main>{children}</main>
+      {/* <button onClick={(() => setMode(!mode))}><h2>toggle</h2></button> */}
       <Footer />
-    </>
+    </div>
   )
 }
